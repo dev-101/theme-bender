c10dfedee7482d234b5b9db40f5145ca7e879254
dev101-osclass <businesss.mail@gmail.com>
2014-06-17 22:39:45 +0200

    update Bender theme version in functions.php

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@
 DEFINES
 
 */
-    define('BENDER_THEME_VERSION', '1.0.1');
+    define('BENDER_THEME_VERSION', '1.1.0');
     if( !osc_get_preference('keyword_placeholder', 'bender_theme') ) {
         osc_set_preference('keyword_placeholder', __('ie. PHP Programmer', 'bender'), 'bender_theme');
     }
